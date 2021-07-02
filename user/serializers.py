fc38616103220b1ac714586a9a926f30bddf41bc
usernamealexgit config --global user.email shpilevoy29@gmail.comgit commit -mcomment <shpilevoy29@gmai.com>
2021-07-02 08:45:31 +0300

    imlemented_users_api

---
+++ user/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+
+from user.models import Profile
+
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = "__all__"
+
+
+class UserSerializer(serializers.ModelSerializer):
+    profile = UserProfileSerializer(read_only=True)
+
+    class Meta:
+        model = User
+        fields = "__all__"
